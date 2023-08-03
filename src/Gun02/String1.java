c172169f9424506098f51b39992c2c02529c005a
TechnoStudy <ismettechno@gmail.com>
2023-08-03 19:06:26 +0100

    Gun02 bitti

---
+++ src/Gun02/String1.java
@@ -0,0 +1,11 @@
+package Gun02;
+
+public class String1 {
+    public static void main(String1[] args) {
+
+        String cumle="Bugün hava çok güzel";
+
+        System.out.println("cumle.length() = " + cumle.length());
+
+    }
+}
