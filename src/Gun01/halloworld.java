549b49c048e1550a0abbcd3d50fcd9f5f8189928
nesterch <terlemez.neslihan@gmail.com>
2023-08-07 16:58:20 +0200

    yeniden deniyorum

--- src/Gun01/halloworld.java
+++ src/Gun01/halloworld.java
@@ -3,5 +3,6 @@ package Gun01;
 public class halloworld {
     public static void main(String[] args) {
         System.out.println("hallo world");
+        System.out.println("how are you");
     }
 }
