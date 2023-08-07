00e11e7e933dfac0ae9e76eb842cc7284a3632f9
nesterch <terlemez.neslihan@gmail.com>
2023-08-07 16:42:42 +0200

    ilk dosya gonderme denemesi

---
+++ src/Gun01/MerhbaDunya.java
@@ -0,0 +1,8 @@
+package Gun01;
+
+public class MerhbaDunya {
+    public static void main(String[] args) {
+        System.out.println("merhaba dunyalilar");
+        System.out.println(" ");
+    }
+}
